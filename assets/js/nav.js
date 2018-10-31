5949acdd214051a09b57c6a232db27d078d10f96
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-10-31 22:27:26 +0000

    Added IDs for buttons

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $("button").click(function() {
-        $("#map").toggleClass("addMargins");
-        $("#controls").toggleClass("removeMargins");
-        $("#locationField").toggleClass("removeMargins");
-        $("#listing").toggleClass("removeMargins");
+    $("#button").click(function() {
+        $("#map").toggleClass("addMargins", 0.1);
+        $("#controls").toggleClass("removeMargins", 0.1);
+        $("#locationField").toggleClass("removeMargins", 0.1);
+        $("#listing").toggleClass("removeMargins", 0.1);
     });
 });

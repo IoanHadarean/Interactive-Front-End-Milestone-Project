015d0e96e78d3567c48954cc1424f302bb7fd491
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-09 11:24:57 +0000

    Removed unnecessary classes and code, fixed nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -4,9 +4,6 @@
 
 $(document).ready(function() {
     $("#button").click(function() {
-        $("#controls").toggleClass("newMargins", 0.1);
-        $("#locationField").toggleClass("newMargins", 0.1);
-        $("#listing").toggleClass("newMargins", 0.1);
         $("#placestoggle").toggleClass("placesMargins", 0.1);
     });
 });

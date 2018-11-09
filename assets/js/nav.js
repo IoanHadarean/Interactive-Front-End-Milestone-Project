1196d38c22ae15edb034acd6150d15cfd0ae5533
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-09 11:30:21 +0000

    Added further fixes for place margins

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,5 @@
 // Function for resizing the menu on toggle when viewed in mobile
-// Adds necessary margins for controls, locationField, listing and 
-// places when the menu is toggled
+// Added necessary margins for places when the menu is toggled
 
 $(document).ready(function() {
     $("#button").click(function() {

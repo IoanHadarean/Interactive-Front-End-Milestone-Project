901e11c97884f9331e65bed5715adccde32bfa42
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-10 23:17:55 +0000

    Fixed radio button bug when the location field is empty

--- assets/js/radio.js
+++ assets/js/radio.js
@@ -11,11 +11,25 @@ $(document).ready(function() {
         // Output the value of the checked radio
         console.log($checked.val());
     });
-    
+
     // Function for clearing out the location field when a new country is selected
     const autoComplete = document.querySelector("#autocomplete");
     const country = document.querySelector('#country');
     country.addEventListener('change', () => {
         autoComplete.value = '';
+    });
+
+    // Function for disabling radio buttons when the location field is empty
+    
+    $('input[type="text"]').on('input propertychange paste', function(e) {
+
+        if ($('#autocomplete').val().length > 0) {
+
+            $('input[type="radio"]').prop('disabled', false);
+
+        }
+        else {
+            $('input[type="radio"]').prop('disabled', true);
+        }
     });
 });

57d4e4305b155c11ec890e58d20a94437a1c2142
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-12 14:31:17 +0000

    Fixed sticky navbar on scroll

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,12 +1,12 @@
-window.onscroll = function() {scroll()};
-var navbar = document.getElementById("navbar");
+window.onscroll = function() { scroll() };
+var navbar = document.getElementById("navbar-default");
 var navbarFixed = navbar.offsetTop;
 
 function scroll() {
-    if(window.pageYOffset >= navbarFixed) {
-       navbar.classList.add("navbarFixed");
-       $("#places").removeClass("placesMargins");
-    } else {
-       navbar.classList.remove("navbarFixed");
+    if (window.pageYOffset >= navbarFixed) {
+        navbar.classList.add("navbarFixed");
+    }
+    else {
+        navbar.classList.remove("navbarFixed");
     }
 }

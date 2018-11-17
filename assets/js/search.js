c3a6feb4316bfcf1518afb692a12eeedfcec5168
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-11-17 15:23:14 +0000

    Updated search.js file

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,17 +1,5 @@
-//Function for filtering the radio buttons by type checked
-
 $(document).ready(function() {
-    var $radio = $('input[name="option"]');
-    //Output the value of each input of type radio
-    console.log($(this).val());
-    $radio.change(function() {
-        var $checked = $radio.filter(function() {
-            return $(this).prop('checked');
-        });
-        // Output the value of the checked radio
-        console.log($checked.val());
-    });
-
+    
     // Function for clearing out the location field when a new country is selected
     var autoComplete = document.querySelector("#autocomplete");
     var country = document.querySelector('#country');

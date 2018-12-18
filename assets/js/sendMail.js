4d91094c22143a51aaf5e5dadca25d1577da0b52
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-18 17:41:53 +0000

    Added asynchronous function to sendMail

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -7,16 +7,29 @@ function sendMail(form) {
             "from_email": form.emailaddress.value,
             "project_request": form.enquiry.value
         })
-        .then(
-            function(response) {
-                console.log("SUCCES", response);
-                alert("Your message has been sent successfully");
-                document.getElementById('form').reset();
-            },
-            function(error) {
-                console.log("FAILED", error);
-                alert("Message was not sent");
-                document.getElementById('form').reset();
-            });
+    .then(
+        function(response) {
+            console.log("SUCCES", response);
+            alert("Your message has been sent successfully");
+            document.getElementById('form').reset();
+        },
+        function(error) {
+            console.log("FAILED", error);
+            alert("Message was not sent");
+            document.getElementById('form').reset();
+        });
     return false;
 }
+
+
+async function init() {
+    await sendMail();
+}
+
+init();
+
+
+
+
+
+
